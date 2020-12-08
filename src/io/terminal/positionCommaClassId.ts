f398d7a9708a657c6e7895000580b201861a7c97
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-07 17:58:57 -0800

    use JI_NOTATION_COMMA_CLASS_IDS

--- src/io/terminal/positionCommaClassId.ts
+++ src/io/terminal/positionCommaClassId.ts
@@ -1,12 +1,12 @@
 import {areScamonsEqual, isUndefined, Maybe, Scamon} from "@sagittal/general"
-import {CommaClassId, getCommaClass, JI_NOTATION} from "@sagittal/system"
+import {CommaClassId, getCommaClass, JI_NOTATION_COMMA_CLASS_IDS} from "@sagittal/system"
 
 const computePositionCommaClassId = (position: Maybe<Scamon<{rational: true}>>): Maybe<CommaClassId> => {
     if (!position) {
         return undefined
     }
 
-    const commaClassId = JI_NOTATION.find((commaClassId: CommaClassId): boolean => {
+    const commaClassId = JI_NOTATION_COMMA_CLASS_IDS.find((commaClassId: CommaClassId): boolean => {
         const commaClass = getCommaClass(commaClassId)
 
         return areScamonsEqual(commaClass.pitch, position)
